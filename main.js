1d17184dc0d27a9e3073bba98182c1fa81b9008f
rachiee02 <charlotteaurora7@gmail.com>
2024-10-31 22:48:00 +0800

    commit

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to("#icecream-almond", {
   y: 100,  // Move down by 100px
   scrollTrigger: {
-    trigger: ".hero-section",  // The section triggering the scroll
+    trigger: ".hero",  // The section triggering the scroll
     start: "top center",       // When the top of the hero section hits the center of the viewport
     scrub: true,               // Smooth scrolling effect
   }
 });
 
 gsap.to("#icecream-classic", {
-  y: -100,  // Move up by 100px
+  y: -20,  // Move up by 20px
   scrollTrigger: {
-    trigger: ".hero-section",
+    trigger: ".hero",
     start: "top center",
     scrub: true,
   }
@@ -23,9 +23,26 @@ gsap.to("#icecream-classic", {
 gsap.to("#icecream-white", {
   y: 150,  // Move down by 150px
   scrollTrigger: {
-    trigger: ".hero-section",
+    trigger: ".hero",
     start: "top center",
     scrub: true,
   }
 });
 
+gsap.to("#icecream-classic2", {
+  y: -50,  // Move up by 50px
+  scrollTrigger: {
+    trigger: ".slogan",
+    start: "top center",
+    scrub: true,
+  }
+});
+
+gsap.to("#icecream-almond2", {
+  y: 20,  // Move down by 20px
+  scrollTrigger: {
+    trigger: ".slogan",
+    start: "top center",
+    scrub: true,
+  }
+});
